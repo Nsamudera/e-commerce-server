5f817196b047eda7071a73ef2c28af0c908dd86b
Nsamudera <nsamudera12@gmail.com>
2018-11-07 13:10:41 +0700

    deleted mongose connection on model

--- models/customer.js
+++ models/customer.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/shopping-cart', {useNewUrlParser:true});
 
 const Schema = mongoose.Schema
 const customerSchema = new Schema({
